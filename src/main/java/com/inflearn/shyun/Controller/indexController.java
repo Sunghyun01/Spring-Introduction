c1a32e21fd283f13c7705d4866121ac87c108194
Sunghyun01 <17ubi23@gmail.com>
2022-11-04 16:33:27 +0900

    API 방식 추가

--- src/main/java/com/inflearn/shyun/Controller/indexController.java
+++ src/main/java/com/inflearn/shyun/Controller/indexController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class indexController {
@@ -19,5 +20,32 @@ public class indexController {
 		model.addAttribute("name",name);
 		
 		return "mvc";
+	}
+	
+	@GetMapping("string")
+	@ResponseBody // body 부분에 아래 내용을 넣어줌
+	//API 방식
+	public String string (@RequestParam(value = "name", required = false) String name) {
+
+		return "ㅁㄴㅇㄻㄴㅇㄹ";
+	}
+	
+	@GetMapping("api")
+	@ResponseBody // body 부분에 아래 내용을 넣어줌
+	public Hello helloapi(@RequestParam(value = "name", required = false) String name) {
+		Hello hello = new Hello();
+		hello.setName(name);
+		
+		return hello;
+	}
+	
+	static class Hello {
+		private String name;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
 	}
 }
