17fa89ef3baf8ff4097a808d505989d783939372
Sunghyun01 <17ubi23@gmail.com>
2022-11-04 15:33:14 +0900

    get방식 Request param 사용

--- src/main/java/com/inflearn/shyun/Controller/indexController.java
+++ src/main/java/com/inflearn/shyun/Controller/indexController.java
@@ -3,13 +3,21 @@ package com.inflearn.shyun.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class indexController {
 	
 	@GetMapping("hello")
 	public String hello (Model model) {
-		model.addAttribute("data","hello");
+		model.addAttribute("data","hello!!");
 		return "hello";
+	}
+	
+	@GetMapping("mvc")
+	public String mvc (@RequestParam(value = "name", required = false) String name, Model model) {
+		model.addAttribute("name",name);
+		
+		return "mvc";
 	}
 }
