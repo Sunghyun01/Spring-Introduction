ff6d67147f87e292a90044c87111a1cc9907017f
Sunghyun01 <17ubi23@gmail.com>
2022-11-16 16:06:19 +0900

    H2 DB 연결

---
+++ src/main/java/hello/hellospirng/SpringConfig.java
@@ -0,0 +1,32 @@
+package hello.hellospirng;
+
+import hello.hellospirng.repository.*;
+import hello.hellospirng.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+	private final DataSource dataSource;
+	
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+    	return new JDBCMemberRepository(dataSource);
+
+    }
+}
