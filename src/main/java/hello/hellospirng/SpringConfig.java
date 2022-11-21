65f0aa5fa6bd8f7c85041b03894f79115b179d1c
Sunghyun01 <17ubi23@gmail.com>
2022-11-21 15:42:39 +0900

    JPA

--- src/main/java/hello/hellospirng/SpringConfig.java
+++ src/main/java/hello/hellospirng/SpringConfig.java
@@ -6,28 +6,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-	private final DataSource dataSource;
-	
+	//private final DataSource dataSource;
+	//private EntityManager em;
+	private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+       // this.dataSource = dataSource;
+        //this.em = em;
+    	this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
     	//return new MemberRepository();
     	//return new JDBCMemberRepository(dataSource);
-    	return new JDBCTemplateMemberRepository(dataSource);
-    }
+    	//return new JDBCTemplateMemberRepository(dataSource);
+//    	return new JpaMemberRepository(em);
+//    }
 }
--- src/main/java/hello/hellospirng/domain/Member.java
+++ src/main/java/hello/hellospirng/domain/Member.java
@@ -1,7 +1,15 @@
 package hello.hellospirng.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	//@Column(name = "컬럼명") // 컬럼명이 다를경우 해당 문법으로 매핑 가능
 	private String name;
 	
 	public long getId() {
